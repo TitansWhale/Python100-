bc80b741b17bace693ffe4dcc7b4f7ab4d124ffd
titanswhale <1784431071@qq.com>
2019-03-29 19:12:25 +0800

    题目：两个乒乓球队进行比赛，各出三人。甲队为a,b,c三人，乙队为x,y,z三人。已抽签决定比赛名单。有人向队员打听比赛的名单。a说他不和x比，c说他不和x,z比，请编程序找出三队赛手的名单。

--- pytest/pythonExample/example22.py
+++ pytest/pythonExample/example22.py
@@ -7,4 +7,4 @@ for i in range(ord('x'),ord('z') + 1):
             for k in range(ord('x'),ord('z') + 1):
                 if (i != k) and (j != k):
                     if (i != ord('x')) and (k != ord('x')) and (k != ord('z')):
-                        print 'order is a -- %s\t b -- %s\tc--%s' % (chr(i),chr(j),chr(k))
+                        print ('order is a -- %s\t b -- %s\tc--%s' % (chr(i),chr(j),chr(k)))
